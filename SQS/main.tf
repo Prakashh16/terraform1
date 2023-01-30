c468c8ba3de7ce8ad5ec4f2f28e038a5dab130b2
Prakashh16 <mishraprakash1230@gmail.com>
2023-01-30 13:52:35 +0530

    add

---
+++ SQS/main.tf
@@ -0,0 +1,43 @@
+resource "aws_sqs_queue" "queue" {
+    name = "s3-event-notification-queue"
+
+    policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "sqs:SendMessage",
+            "Resource": "arn:aws:sqs:*:*:s3-event-notification-queue",
+            "Condition": {
+                "ArnEquals": { "aws:SourceArn": "${aws_s3_bucket.bucket.arn}" }
+            }
+        }
+    ]
+}    
+POLICY  
+}
+
+resource "random_string" "id" {
+    length = "5"
+    special = false
+    upper   = false
+  
+}
+
+resource "aws_s3_bucket" "bucket" {
+    bucket = "mybucket-s3-g2-${random_string.id.result}"
+  
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+    # count  = "${var.events ? 1 : 0 }"
+    bucket = "${aws_s3_bucket.bucket.id}" 
+
+    queue {
+      queue_arn = "${aws_sqs_queue.queue.arn}"
+      events    = ["s3:ObjectCreated:Put"]
+    }
+  
+}
